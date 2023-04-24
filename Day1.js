764ccd61d5ce9421171733675a2b839e142437da
navalraul <129669942+navalraul@users.noreply.github.com>
2023-04-24 17:42:11 +0530

    update

---
+++ Day1.js
@@ -0,0 +1,19 @@
+// var a = "cksdj";
+// console.log(a)
+// console.log(typeof(a))
+// var b = 1234;
+// console.log(b)
+// console.log(typeof(b))
+// var c = true;
+// console.log(c)
+// console.log(typeof(c))
+// var d ;
+// console.log(d)
+// console.log(typeof(d))
+
+var a = 12345
+// console.log(a)
+var b = 98745
+// console.log(b)
+var add = (a*b)
+console.log(add)
