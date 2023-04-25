8c38409a1de88da770ddeba9f638c32e99c3246e
navalraul <129669942+navalraul@users.noreply.github.com>
2023-04-25 17:12:40 +0530

    update

--- Day1.js
+++ Day1.js
@@ -11,9 +11,116 @@
 // console.log(d)
 // console.log(typeof(d))
 
-var a = 12345
-// console.log(a)
-var b = 98745
-// console.log(b)
-var add = (a*b)
-console.log(add)
+// var a = 12345
+// // console.log(a)
+// var b = 98745
+// // console.log(b)
+// var add = (a*b)
+// console.log(add)
+
+
+// Write a program that takes in two integers and prints out their
+// sum, difference, product, and quotient.
+
+
+// var a = 10;
+// var b = 2;
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+
+// Write a program that takes in an integer and checks if it's even or odd.
+
+// var num = 21;
+
+// if (num%2 == 0) {
+//     console.log("Number is even")
+// }else {
+//     console.log("Number is odd")
+// }
+
+// Write a program that takes in a character and checks if it's a letter or a digit.
+
+// var multi = 2;
+
+// if (multi%3==0 && multi%5== 0) {
+//     console.log("print it is divisible")
+// } else {
+//     console.log("print it is not divisible")
+// }
+
+// var a = 5;
+// var b = 8;
+
+// if ((a+b)%2 == 0) {
+//     console.log("number is even")
+// }else {
+//     console.log("number is odd")
+// }
+
+
+// var far = 15
+
+// console.log(far*9/5+32)
+
+// var a = 5;
+// var b = 4;
+// var c = 8;
+
+// if (a>b || a > c) {
+//     console.log("a is greater")
+// }else if (b >a && b >c) {
+//     console.log("b is greater")
+// }else {
+//     console.log("c is greater")
+// }
+
+// var a = 100;
+// var b = 20;
+
+// if ((a+b) > 100) {
+//     console.log("number is greater")
+// }else {
+//     console.log("number is smaller")
+// }
+
+// var year = 2032;
+
+// if (year%4==0 && year%100==0 && year%400==0) {
+//     console.log("it is a leap year")
+// }else {
+//     console.log("it is not a leap year")
+// }
+
+// var name = "navnath"
+
+// var find = 1234;
+
+// if ()
+
+
+// var a = 10;
+// var b = 15;
+// var c = 2;
+
+// if ((a > b && a> c)&&(a<b<c)) {
+//     console.log(a, "is greater",a,"a is smaller")
+// }else if ((b>a && b>c)&&(b<c<a)) {
+//     console.log(b, "is greater",b,"b is smaller")
+// }else if ((c>a && c>b)&&(c<b<a)) {
+//     console.log( c,"is greater",c,"c is smaller")
+// }else {
+//     console.log("invalid")
+// }
+
+var let = "P";
+
+if (let>= "a" && let<="z") {
+    console.log("Lowercase")
+} else if (let >="A" && let<="Z") {
+    console.log("Uppercase")
+}else {
+    console.log("Invalid")
+}
